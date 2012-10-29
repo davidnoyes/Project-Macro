4b1f5cfc0d79c35b084fdd88edec5351662138d3
raspberrypython <david.j.noyes@gmail.com>
2012-10-29 00:33:59 +0000

    -Disabled admin docs. -Organised common files to pisite.

--- pisite/settings.py
+++ pisite/settings.py
@@ -69,7 +69,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'static_files/'),
+    os.path.join(SITE_ROOT, 'pisite/static_files/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -118,8 +118,9 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
+    #'django.contrib.admindocs',
     'gunicorn',
+    'pisite',
     'macro',
 )
 
--- pisite/urls.py
+++ pisite/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', redirect_to, {'url': '/macro'}),
     url(r'^macro/', include('macro.urls')),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
